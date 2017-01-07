78824e889009ffcb25db53ac51c5512a0c52565a
Nicolas Rabecq <nicotigri@hotmail.fr>
2017-01-07 15:16:34 +0100

    first query failed

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  //host     : 'mysql51-97.perso',
-  host     : 'localhost',
-  user     : 'nicolasryft9216',
-  password : 'kADhhdQg',
-  database : 'magic-the-gathering'
+    //host     : 'mysql51-97.perso',
+    host     : '127.0.0.1',
+    port     : '8888',
+    user     : 'nicolasryft9216',
+    password : 'kADhhdQg',
+    database : 'magic-the-gathering'
 });
 
-connection.connect();
+connection.connect(function(err){
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    } 
+    console.log('connected as id ' + connection.threadId);
+});
 
-connection.query('SELECT * from < table name >', function(err, rows, fields) {
-  if (!err)
-    console.log('The solution is: ', rows);
-  else
-    console.log('Error while performing Query.');
+connection.query('SELECT * FROM edition WHERE 1', function(err, rows) {
+    if(!err) {
+        console.log(rows);
+    } else {
+        console.error(err);
+    }
 });
 
 connection.end();
