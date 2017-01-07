05418540d354d5359a64e86cfe936aceb5e78a63
Nicolas Rabecq <nicotigri@hotmail.fr>
2017-01-07 15:48:56 +0100

    first query works fine

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    //host     : 'mysql51-97.perso',
-    host     : '127.0.0.1',
-    port     : '8888',
-    user     : 'nicolasryft9216',
-    password : 'kADhhdQg',
-    database : 'magic-the-gathering'
-});
+var express    = require('express');
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./REST.js");
+var app  = express();
 
-connection.connect(function(err){
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    } 
-    console.log('connected as id ' + connection.threadId);
-});
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
 
-connection.query('SELECT * FROM edition WHERE 1', function(err, rows) {
-    if(!err) {
-        console.log(rows);
-    } else {
-        console.error(err);
-    }
-});
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        //host     : 'mysql51-97.perso',
+        host     : '127.0.0.1',
+        port     : 8889,
+        user     : 'nicolasryft9216',
+        password : 'kADhhdQg',
+        database : 'magic-the-gathering',
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
+}
 
-connection.end();
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
+}
+
+REST.prototype.startServer = function() {
+      app.listen(3001,function(){
+          console.log("All right ! I am alive at Port 3001.");
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new REST();
